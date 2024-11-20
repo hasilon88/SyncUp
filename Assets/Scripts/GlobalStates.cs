73fe6f42e2562fa586c278307c14517c8e4783d1
olvrMns <oliviermansuy7@gmail.com>
2024-11-20 13:27:26 -0500

    globalStates ability / AbilitySource tag

--- Assets/Scripts/GlobalStates.cs
+++ Assets/Scripts/GlobalStates.cs
@@ -12,6 +12,8 @@ public class GlobalStates : MonoBehaviour
     public int CurrentLevel = 1;
     public int ScaledTime = 0;
     public float RealTime = 0f;
+    public Ability FirstAbility;
+    public Ability SecondAbility;   
 
     private void Awake()
     {
