5b980345a5183e68203e29de6a509e1e52b49794
olvrMns <oliviermansuy7@gmail.com>
2024-11-20 13:34:25 -0500

    removed test...

--- Assets/Scripts/Ability/Rewind/PlayerRewinder.cs
+++ Assets/Scripts/Ability/Rewind/PlayerRewinder.cs
@@ -10,7 +10,6 @@ public class PlayerRewinder : IRewind
     public override void ResetRewindProperties()
     {
         rewindOffset = 0;
-        Debug.Log(" TEST : " + rewindAbility.GetRewindDurationInFrames());
         lastEulerAngles = new RewindArray<Vector3>(rewindAbility.GetRewindDurationInFrames());
         lastPositions = new RewindArray<Vector3>(rewindAbility.GetRewindDurationInFrames());
     }
