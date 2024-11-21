1bf5fb41cce7172ef6046cc7ab01a0b91a8ac016
olvrm <oliviermansuy7@gmail.com>
2024-11-21 14:18:25 -0500

    Spinnign lab

---
+++ Assets/Scripts/StructureSpin.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureSpin : MonoBehaviour
+{
+
+    public Camera Camera;
+    public float Speed = 5f;
+
+
+    void Update()
+    {
+        transform.RotateAround(Camera.transform.position, Vector3.up, Speed * Time.deltaTime);
+    }
+}
